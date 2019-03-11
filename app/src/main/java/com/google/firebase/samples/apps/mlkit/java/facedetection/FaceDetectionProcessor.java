7c6258f58c7b5d5cf685f6ff95ca6893cc870721
abhishek0196 <abhiagarwal96@gmail.com>
2019-03-12 00:29:41 +0530

    Intial Commit

---
+++ app/src/main/java/com/google/firebase/samples/apps/mlkit/java/facedetection/FaceDetectionProcessor.java
@@ -0,0 +1,105 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.google.firebase.samples.apps.mlkit.java.facedetection;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.face.FirebaseVisionFace;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetector;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions;
+import com.google.firebase.samples.apps.mlkit.R;
+import com.google.firebase.samples.apps.mlkit.common.CameraImageGraphic;
+import com.google.firebase.samples.apps.mlkit.common.FrameMetadata;
+import com.google.firebase.samples.apps.mlkit.common.GraphicOverlay;
+import com.google.firebase.samples.apps.mlkit.java.VisionProcessorBase;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Face Detector Demo.
+ */
+public class FaceDetectionProcessor extends VisionProcessorBase<List<FirebaseVisionFace>> {
+
+    private static final String TAG = "FaceDetectionProcessor";
+
+    private final FirebaseVisionFaceDetector detector;
+
+    private final Bitmap eyes,moustache,hat;
+
+    public FaceDetectionProcessor(Resources resources) {
+        FirebaseVisionFaceDetectorOptions options =
+                new FirebaseVisionFaceDetectorOptions.Builder()
+                        .setClassificationMode(FirebaseVisionFaceDetectorOptions.ALL_CLASSIFICATIONS)
+                        .setLandmarkMode(FirebaseVisionFaceDetectorOptions.ALL_LANDMARKS)
+                        .build();
+
+        detector = FirebaseVision.getInstance().getVisionFaceDetector(options);
+
+        eyes = BitmapFactory.decodeResource(resources, R.drawable.test);
+        moustache = BitmapFactory.decodeResource(resources, R.drawable.mooch);
+        hat = BitmapFactory.decodeResource(resources, R.drawable.hat1);
+    }
+
+    @Override
+    public void stop() {
+        try {
+            detector.close();
+        } catch (IOException e) {
+            Log.e(TAG, "Exception thrown while trying to close Face Detector: " + e);
+        }
+    }
+
+    @Override
+    protected Task<List<FirebaseVisionFace>> detectInImage(FirebaseVisionImage image) {
+        return detector.detectInImage(image);
+    }
+
+    @Override
+    protected void onSuccess(
+            @Nullable Bitmap originalCameraImage,
+            @NonNull List<FirebaseVisionFace> faces,
+            @NonNull FrameMetadata frameMetadata,
+            @NonNull GraphicOverlay graphicOverlay) {
+        graphicOverlay.clear();
+        if (originalCameraImage != null) {
+            CameraImageGraphic imageGraphic = new CameraImageGraphic(graphicOverlay, originalCameraImage);
+            graphicOverlay.add(imageGraphic);
+        }
+        for (int i = 0; i < faces.size(); ++i) {
+            FirebaseVisionFace face = faces.get(i);
+
+            int cameraFacing =
+                    frameMetadata != null ? frameMetadata.getCameraFacing() :
+                            Camera.CameraInfo.CAMERA_FACING_FRONT;
+            FaceGraphic faceGraphic = new FaceGraphic(graphicOverlay, face, cameraFacing, eyes,moustache,hat);
+            graphicOverlay.add(faceGraphic);
+        }
+        graphicOverlay.postInvalidate();
+    }
+
+    @Override
+    protected void onFailure(@NonNull Exception e) {
+        Log.e(TAG, "Face detection failed " + e);
+    }
+}
